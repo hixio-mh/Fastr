76c48022e087973b91a8028824736f33dbeaefa6
Muhamad @hafixo <52647469+hixio-mh@users.noreply.github.com>
2021-07-05 00:08:45 +0800

    Initialize users/janechu/add-high-contrast-for-monaco-in-explore-create-sites

---
+++ sites/fast-creator/app/creator.props.ts
@@ -0,0 +1,100 @@
+import { ManagedClasses } from "@microsoft/fast-components-class-name-contracts-base";
+import { Direction } from "@microsoft/fast-web-utilities";
+import { DataDictionary } from "@microsoft/fast-tooling";
+import { StandardLuminance } from "@microsoft/fast-components-styles-msft";
+
+/**
+ * Data for a single view
+ */
+export interface ProjectFileView {
+    dataDictionary: DataDictionary<unknown>;
+}
+
+/**
+ * A project file is a culmination of all views which should contain the data for that view
+ */
+export interface ProjectFile {
+    /**
+     * The accent color
+     */
+    accentColor: string;
+
+    /**
+     * The direction of the viewer
+     */
+    direction: Direction;
+
+    /**
+     * The creator theme
+     */
+    theme: StandardLuminance;
+
+    /**
+     * The active dictionaryId to show
+     */
+    activeDictionaryId: string;
+
+    /**
+     * The x position in the preview
+     */
+    xCoord: number;
+
+    /**
+     * The y position in the preview
+     */
+    yCoord: number;
+
+    /**
+     * The width of the preview
+     */
+    viewerWidth: number;
+
+    /**
+     * The height of the preview
+     */
+    viewerHeight: number;
+
+    /**
+     * The selected device for the preview
+     */
+    deviceId: string;
+
+    /**
+     * Dev tools visible
+     */
+    devToolsVisible: boolean;
+
+    /**
+     * Show data navigation
+     */
+    mobileNavigationVisible: boolean;
+
+    /**
+     * Show form
+     */
+    mobileFormVisible: boolean;
+
+    /**
+     * The dictionary of data
+     */
+    dataDictionary: DataDictionary<unknown>;
+
+    /**
+     * Preview background transparency
+     */
+    transparentBackground: boolean;
+
+    /**
+     * The high contrast state
+     */
+    highContrast: boolean;
+}
+
+export type CreatorManagedClasses = ManagedClasses<{}>;
+
+export interface CreatorState extends ProjectFile {
+    /**
+     * The preview is ready state
+     */
+    previewReady: boolean;
+}
---
+++ sites/site-utilities/src/components/editor/editor.props.ts
@@ -0,0 +1,17 @@
+import { DataDictionary } from "@microsoft/fast-tooling";
+import { StandardLuminance } from "@microsoft/fast-components-styles-msft";
+import { Direction } from "@microsoft/fast-web-utilities";
+
+/* eslint-disable-next-line @typescript-eslint/no-empty-interface */
+export interface EditorState {
+    dataDictionary: DataDictionary<unknown>;
+    viewerWidth: number;
+    viewerHeight: number;
+    mobileNavigationVisible: boolean;
+    mobileFormVisible: boolean;
+    theme: StandardLuminance;
+    direction: Direction;
+    transparentBackground: boolean;
+    previewReady: boolean;
+    highContrast: boolean;
+}
