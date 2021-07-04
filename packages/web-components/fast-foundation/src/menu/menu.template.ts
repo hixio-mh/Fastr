7f7bed8c0a2bc10dbafd573c4287f3a4729c52a0
Muhamad @hafixo <52647469+hixio-mh@users.noreply.github.com>
2021-07-05 00:12:30 +0800

    Initialize users/robarb/scroller

---
+++ packages/web-components/fast-foundation/src/menu/menu.template.ts
@@ -0,0 +1,17 @@
+import { html, slotted } from "@microsoft/fast-element";
+import { Menu } from "./menu";
+
+/**
+ * The template for the {@link @microsoft/fast-foundation#Menu} component.
+ * @public
+ */
+export const MenuTemplate = html<Menu>`
+    <template
+        slot="${x => (x.isNestedMenu() ? "submenu" : void 0)}"
+        role="menu"
+        @keydown="${(x, c) => x.handleMenuKeyDown(c.event as KeyboardEvent)}"
+        @focusout="${(x, c) => x.handleFocusOut(c.event as FocusEvent)}"
+    >
+        <slot ${slotted("items")}></slot>
+    </template>
+`;
