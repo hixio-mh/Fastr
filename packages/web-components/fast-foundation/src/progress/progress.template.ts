7be80379a72b0f014c7b435ded8f8fc26d596ce3
Muhamad @hafixo <52647469+hixio-mh@users.noreply.github.com>
2021-07-05 00:13:12 +0800

    Initialize users/janechu/add-post-api-creator-2

---
+++ packages/web-components/fast-foundation/src/progress/progress.template.ts
@@ -0,0 +1,47 @@
+import { html, when } from "@microsoft/fast-element";
+import type { ViewTemplate } from "@microsoft/fast-element";
+import type { BaseProgress } from "./base-progress";
+
+/**
+ * The template for the {@link @microsoft/fast-foundation#BaseProgress} component.
+ * @public
+ */
+export const ProgressTemplate: ViewTemplate<BaseProgress> = html`
+    <template
+        role="progressbar"
+        aria-valuenow="${x => x.value}"
+        aria-valuemin="${x => x.min}"
+        aria-valuemax="${x => x.max}"
+        class="${x => (x.paused ? "paused" : "")}"
+    >
+        ${when(
+            x => x.value,
+            html<BaseProgress>`
+                <div class="progress" part="progress" slot="determinate">
+                    <div
+                        class="determinate"
+                        part="determinate"
+                        style="width: ${x => x.value}%"
+                    ></div>
+                </div>
+            `
+        )}
+        ${when(
+            x => !x.value,
+            html<BaseProgress>`
+                <div class="progress" part="progress" slot="indeterminate">
+                    <slot class="indeterminate" name="indeterminate">
+                        <span
+                            class="indeterminate-indicator-1"
+                            part="indeterminate-indicator-1"
+                        ></span>
+                        <span
+                            class="indeterminate-indicator-2"
+                            part="indeterminate-indicator-2"
+                        ></span>
+                    </slot>
+                </div>
+            `
+        )}
+    </template>
+`;
---
+++ sites/fast-creator/app/app.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import Creator from "./creator";
+import Preview from "./preview";
+
+/**
+ * Create the root node
+ */
+export default class App extends React.Component<{}, {}> {
+    public render(): React.ReactNode {
+        return (
+            <BrowserRouter>
+                <fast-design-system-provider use-defaults density="-2">
+                    <Route component={Creator} exact={true} path="/" />
+                    <Route
+                        component={Creator}
+                        exact={true}
+                        path="/data-dictionary/:key"
+                    />
+                    <Route component={Preview} exact={true} path="/preview" />
+                </fast-design-system-provider>
+            </BrowserRouter>
+        );
+    }
+}
