cf898382ce8107293ac2a739b7112a3dd2dfa6b9
Muhamad @hafixo <52647469+hixio-mh@users.noreply.github.com>
2021-07-05 00:03:42 +0800

    Initialize users/v-sedono/add-carousel-wc-component

---
+++ packages/fast-components/src/index.ts
@@ -0,0 +1,13 @@
+export * from "./badge";
+export * from "./card";
+export * from "./checkbox";
+export * from "./design-system-provider";
+export * from "./dialog";
+export * from "./divider";
+export * from "./progress";
+export * from "./switch";
+export * from "./text-area";
+export * from "./text-field";
+
+// export our style goodness
+export * from "./styles";
---
+++ packages/fast-tooling-react/src/form/controls/control.array.style.ts
@@ -0,0 +1,119 @@
+import { ellipsis } from "@microsoft/fast-jss-utilities";
+import { ComponentStyles, CSSRules } from "@microsoft/fast-jss-manager-react";
+import {
+    addItemStyle,
+    cleanListStyle,
+    defaultFontStyle,
+    invalidMessageStyle,
+    labelRegionStyle,
+    labelStyle,
+    removeItemStyle,
+} from "../../style";
+
+/**
+ * Array class name contract
+ */
+export interface ArrayControlClassNameContract {
+    arrayControl?: string;
+    arrayControl__disabled?: string;
+    arrayControl__invalid?: string;
+    arrayControl_invalidMessage?: string;
+    arrayControl_addItem?: string;
+    arrayControl_addItemButton?: string;
+    arrayControl_addItemLabel?: string;
+    arrayControl_existingItemList?: string;
+    arrayControl_existingItemListItem?: string;
+    arrayControl_existingItemListItem__invalid?: string;
+    arrayControl_existingItemListItemLink?: string;
+    arrayControl_existingItemListItemLink__default?: string;
+    arrayControl_existingItemRemoveButton?: string;
+}
+
+const styles: ComponentStyles<ArrayControlClassNameContract, {}> = {
+    arrayControl: {},
+    arrayControl__disabled: {},
+    arrayControl__invalid: {
+        "& $arrayControl_existingItemList": {
+            "&::before": {
+                "border-color": "red",
+            },
+        },
+    },
+    arrayControl_invalidMessage: {
+        ...invalidMessageStyle,
+        "padding-bottom": "5px",
+        "margin-top": "-5px",
+    },
+    arrayControl_addItem: {
+        ...labelRegionStyle,
+        position: "relative",
+    },
+    arrayControl_addItemLabel: {
+        ...labelStyle,
+        "max-width": "calc(100% - 30px)",
+    },
+    arrayControl_addItemButton: {
+        ...addItemStyle,
+    },
+    arrayControl_existingItemList: {
+        ...cleanListStyle,
+        "font-size": "12px",
+        position: "relative",
+        "&::before": {
+            content: "''",
+            display: "block",
+            width: "calc(100% - 30px)",
+            "border-bottom": "1px solid transparent",
+            position: "absolute",
+            top: "0",
+            bottom: "0",
+        },
+    },
+    arrayControl_existingItemListItem: {
+        position: "relative",
+        "padding-left": "10px",
+        "margin-left": "-10px",
+        cursor: "pointer",
+        height: "30px",
+        "line-height": "30px",
+        "margin-bottom": "5px",
+        "border-radius": "2px",
+        "&::before": {
+            content: "''",
+            position: "absolute",
+            "border-radius": "2px",
+            "pointer-events": "none",
+            height: "inherit",
+            width: "calc(100% - 40px)",
+            border: "1px solid transparent",
+        },
+    },
+    arrayControl_existingItemListItem__invalid: {
+        "&::before": {
+            background: "rgba(255,0,0,0.1)",
+            "border-color": "red",
+            "box-sizing": "border-box",
+        },
+    },
+    arrayControl_existingItemListItemLink: {
+        ...ellipsis(),
+        cursor: "pointer",
+        display: "block",
+        height: "30px",
+        "line-height": "30px",
+        width: "calc(100% - 40px)",
+        padding: "0 5px",
+        "background-color": "rgba(255, 255, 255, 0.04)",
+        "&$arrayControl_existingItemListItemLink__default": {
+            ...defaultFontStyle,
+            cursor: "auto",
+        },
+    },
+    arrayControl_existingItemListItemLink__default: {},
+    arrayControl_existingItemRemoveButton: {
+        ...removeItemStyle,
+        cursor: "pointer",
+    },
+};
+
+export default styles;
